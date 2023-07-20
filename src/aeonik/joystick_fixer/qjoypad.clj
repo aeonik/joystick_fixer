0a609d0b8c500f59141a58e034bd80148d6aa7bf
Aeonik Chaos <aeonik.chaos@gmail.com>
2023-07-20 15:32:28 -0400

    Adding QJoypad Parser

---
+++ src/aeonik/joystick_fixer/qjoypad.clj
@@ -0,0 +1,199 @@
+(ns aeonik.joystick-fixer.qjoypad
+  (:require [instaparse.core :as instaparse]
+            [instaparse.combinators :as combinators]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.java.shell :as sh]
+            [clojure.pprint :refer [pprint]]
+            [malli.core :as m]
+            [ubergraph.core :as g])
+
+  (:import [java.nio.file Files Paths]
+           (org.apache.commons.io FileUtils)))
+
+
+(def "~/qjoypad_virpils.lyt")
+
+;; Define a schema for a joystick.
+(def joystick-schema
+  [:map
+   [:joystick-number :int]
+   [:buttons
+    [:vector [:map
+              [:button-number :int]
+              [:type :string]
+              [:type-number :int]]]]])
+
+(def joystick-test-data
+  {:joystick-number 4
+   :buttons [{:button-number 22, :type "key", :type-number 67}
+             {:button-number 30, :type "key", :type-number 64}]})
+(m/validate joystick-schema joystick-test-data)
+
+(def file (slurp "/home/dave/qjoypad_virpils.lyt"))
+
+(def joystick-tranforms {1 2
+                         2 3
+                         3 1
+                         4 4})
+(def qjoypad-ebnf "file = header joystick+
+                   header = '# QJoyPad 4.3 Layout File' <newlines>
+                   joystick = <'Joystick'> <space> joystick_number <space> <'{'> <newlines> key_map?+ <'}'> <newlines>
+                   key_map = <tab> joystick_button type <newlines>?
+                   joystick_button = <'Button'> <space> number <':'> <space>
+                   type =  (#'\\w+' <space> type_button)
+                   type_button = number
+                   <space> = ' '
+                   <newlines> = #'\\n+'
+                   <tab> = #'\\t'
+                   <joystick_number> = number
+                   <number> = #'[0-9]+'")
+(def qjoypad-parser (instaparse/parser qjoypad-ebnf :output-format :enlive))
+
+(combinators/ebnf qjoypad-ebnf)
+(def qjoypad-grammar-combinator
+  {:file (combinators/cat
+           (combinators/nt :header)
+           (combinators/plus (combinators/nt :joystick)))
+   :header (combinators/cat
+             (combinators/string "# QJoyPad 4.3 Layout File")
+             (combinators/nt :newlines))
+   :joystick (combinators/cat
+               (combinators/string "Joystick")
+                (combinators/nt :space)
+               (combinators/nt :joystick_number)
+               (combinators/nt :space)
+               (combinators/string "{")
+               (combinators/nt :newlines)
+               (combinators/star (combinators/nt :key_map))
+               (combinators/string "}")
+               (combinators/nt :newlines))
+   :key_map (combinators/cat
+              (combinators/nt :tab)
+              (combinators/nt :joystick_button)
+              (combinators/nt :type)
+              (combinators/opt (combinators/nt :newlines)))
+   :joystick_button (combinators/cat
+                      (combinators/string "Button")
+                      (combinators/nt :space)
+                      (combinators/nt :number)
+                      (combinators/string ":")
+                      (combinators/nt :space))
+   :type (combinators/cat
+           (combinators/regexp "\\w+")
+           (combinators/nt :space)
+           (combinators/nt :type_button))
+   :type_button (combinators/nt :number)
+   :joystick_number (combinators/nt :number)
+   :number (combinators/regexp "\\d+")
+   :tab (combinators/string "\t")
+   :newlines (combinators/regexp "\\n+")
+   :space (combinators/string " ")})
+
+(def qjoypad-parser-combinator (instaparse/parser qjoypad-grammar-combinator :start :file :output-format :enlive))
+
+(def transformations
+  "Can be to remap the entire structure to a map."
+  {:file            (fn [header & joysticks] {:header header, :joysticks joysticks})
+   :joystick        (fn [number & key_maps] {:joystick-number number, :key-maps key_maps})
+   :key_map         (fn [joystick-button & types] {:joystick-button joystick-button, :types types})
+   :joystick_button (fn [number] {:button-number number})
+   :type            (fn [type-name & type-buttons] {:type-name type-name, :type-buttons type-buttons})
+   :type_button     (fn [number] {:type-button-number number})})
+
+(def parse-tree (instaparse/parse qjoypad-parser file :unhide :all))
+(def parse-tree-combinator (instaparse/parse qjoypad-parser-combinator file :unhide :all))
+
+(instaparse/transform transformations parse-tree)
+
+(defn concatenate-tree
+  "Unparses the tree"
+  [tree]
+  (cond
+    (map? tree) (concatenate-tree (get tree :content))
+    (coll? tree) (reduce str (map concatenate-tree tree))
+    :else tree))
+
+(def unparsed-tree (concatenate-tree parse-tree))
+
+;; Compare strings
+(= file unparsed-tree)
+
+(comment [:file
+          [:header "# QJoyPad 4.3 Layout File"]
+          [:joystick
+           "4"
+           [:key_map [:joystick_button "22"] [:type "key" [:type_button "67"]]]
+           [:key_map [:joystick_button "30"] [:type "key" [:type_button "64"]]]]
+          [:joystick "1"]
+          [:joystick "3"]
+          [:joystick
+           "2"
+           [:key_map [:joystick_button "27"] [:type "key" [:type_button "34"]]]
+           [:key_map [:joystick_button "28"] [:type "key" [:type_button "35"]]]]
+          [:joystick
+           "5"
+           [:key_map [:joystick_button "3"] [:type "mouse" [:type_button "1"]]]
+           [:key_map [:joystick_button "4"] [:type "mouse" [:type_button "3"]]]
+           [:key_map [:joystick_button "30"] [:type "key" [:type_button "41"]]]
+           [:key_map [:joystick_button "39"] [:type "key" [:type_button "67"]]]
+           [:key_map [:joystick_button "40"] [:type "key" [:type_button "68"]]]]])
+
+
+;; Trying to come up with a way to transform arbitrary grammars
+(defn transform-joystick [joystick-transforms tree]
+  (clojure.walk/postwalk
+    (fn [node]
+      (if (and (vector? node) (= :joystick (first node)))
+        (let [old-joystick-number (second node)
+              new-joystick-number (joystick-transforms old-joystick-number)]
+          (assoc node 1 new-joystick-number))
+        node))
+    tree))
+
+(transform-joystick joystick-tranforms parse-tree)
+
+;; Text slinging, avoid this if possible, but it can work if the grammar is simple enough, or tolerant to index changes
+(defn replace-text [text start end new-text]
+  (str (subs text 0 start) new-text (subs text end)))
+
+(def metadata {:instaparse.gll/start-index 27, :instaparse.gll/end-index 81})
+(def modified-file-content
+  (replace-text file
+                (:instaparse.gll/start-index metadata)
+                (:instaparse.gll/end-index metadata)
+                "replacement text"))
+
+
+(defn replace-joystick-numbers-in-text [text parse-tree]
+  (let [replacement-ranges (->> parse-tree
+                                (tree-seq vector? seq)
+                                (filter #(= :joystick_number (first %)))
+                                (map instaparse/span))
+        sorted-ranges (sort-by first replacement-ranges)
+        replacements (map (fn [[start end]]
+                            (let [old-number (Integer/parseInt (subs text start end))
+                                  new-number (get joystick-transforms old-number)]
+                              (str new-number)))
+                          sorted-ranges)
+        parts (map (fn [[start end]] (subs text start end))
+                   (partition 2 1 (interleave (cons 0 (map second sorted-ranges)) (cons (dec (count text)) nil))))]
+    (apply str (interleave parts replacements))))
+
+
+;; Print with metadata
+(pprint (meta parse-tree))
+
+;; Get a joystick from the hiccup parse tree using keywords :file and :joystick
+(get-in parse-tree [:header :joystick])
+
+(get-in parse-tree [2])
+(meta (get-in parse-tree [2]))
+
+
+(instaparse/span parse-tree)
+
+(instaparse/add-line-and-column-info-to-metadata file parse-tree)
+(def annotated-tree (instaparse/add-line-and-column-info-to-metadata file parse-tree))
+
+(instaparse/unparse)
