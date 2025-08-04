fdf9aebd89d777d286e77b30cb3f45fbbd79a8fe
Aeonik Chaos <aeonik.chaos@gmail.com>
2025-08-04 16:00:31 -0400

    Gui working with tabs and all svg files

--- src/aeonik/controlmap/gui.clj
+++ src/aeonik/controlmap/gui.clj
@@ -1,27 +1,37 @@
 (ns aeonik.controlmap.gui
   (:require [clojure.string :as str]
+            [clojure.java.io :as io]
             [cljfx.api :as fx]
             [cljfx.ext.web-view :as fx.ext.web-view])
   (:import [javafx.scene.web WebEvent]))
 
-;; Application state
+;; --- Application State ---
+(def svg-files
+  (->> (file-seq (io/file "out"))
+       (filter #(and (.isFile %)
+                     (str/ends-with? (.getName %) ".svg")))
+       (sort-by #(.getName %))
+       vec))
+
 (def *state
   (atom
-   {:title nil
+   {:svg-files svg-files
+    :active-file (first svg-files)
     :status nil}))
 
+;; --- Status Handler ---
 (defn handle-status-change [status]
   (cond
     (str/starts-with? status "clicked:")
     (let [id (subs status (count "clicked:"))]
-      (println "Performing Clojure logic for click on:" id))
+      (println "Clicked:" id))
 
     (str/starts-with? status "hovered:")
     (let [id (subs status (count "hovered:"))]
-      (println "Hover detected on:" id))
+      (println "Hovered:" id))
 
     :else
-    (println "Unhandled status message:" status)))
+    (println "Unknown status message:" status)))
 
 (add-watch *state ::status-watcher
            (fn [_ _ old new]
@@ -30,32 +40,43 @@
                (when (not= old-status new-status)
                  (handle-status-change new-status)))))
 
-;; View function that creates the GUI components
-(defn view [{:keys [title status]}]
+;; --- View Function ---
+(defn view [{:keys [svg-files active-file status]}]
   {:fx/type :stage
    :showing true
-   :title (str title)
+   :title (str (.getName ^java.io.File active-file))
    :scene
    {:fx/type :scene
     :root
-    {:fx/type :v-box
-     :fill-width true
-     :children
-     [{:fx/type fx.ext.web-view/with-engine-props
-       :desc {:fx/type :web-view
-              :max-width Double/MAX_VALUE
-              :max-height Double/MAX_VALUE}
-       :v-box/vgrow :always
-       :props {:url (str "file://" (.getAbsolutePath (java.io.File. "out/updated_vpc_mongoose_t50cm3.svg")))
-               :on-title-changed #(swap! *state assoc :title %)
-               :on-status-changed #(swap! *state assoc :status (.getData ^WebEvent %))}}
-      {:fx/type :label
-       :text (str status)}]}}})
+    {:fx/type :tab-pane
+     :side :top
+     :tabs (mapv
+            (fn [^java.io.File f]
+              {:fx/type :tab
+               :id (.getPath f)
+               :text (.getName f)
+               :closable false
+               :on-selection-changed
+               (fn [e]
+                 (let [^javafx.scene.control.Tab tab (.getSource e)]
+                   (when (.isSelected tab)
+                     (swap! *state assoc :active-file f))))
+               :content
+               {:fx/type fx.ext.web-view/with-engine-props
+                :desc {:fx/type :web-view
+                       :max-width Double/MAX_VALUE
+                       :max-height Double/MAX_VALUE}
+                :props {:url (str "file://" (.getAbsolutePath f))
+                        :on-title-changed (fn [_]) ;; you could update title separately
+                        :on-status-changed
+                        (fn [^WebEvent evt]
+                          (swap! *state assoc :status (.getData evt)))}}})
+            svg-files)}}})
 
-;; Renderer creation
+;; --- Renderer ---
 (def renderer
   (fx/create-renderer
    :middleware (fx/wrap-map-desc #'view)))
 
-;; Mount the renderer to the application state
+;; --- App Start ---
 (fx/mount-renderer *state renderer)
