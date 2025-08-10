6bd4d265bcdfa32d5b180a9f08989386ca428df8
Aeonik Chaos <aeonik.chaos@gmail.com>
2025-08-10 11:36:21 -0400

    Got file:///href working in svg

--- src/aeonik/controlmap/gui.clj
+++ src/aeonik/controlmap/gui.clj
@@ -33,6 +33,19 @@
       (catch Throwable t
         (println "Dock icon set failed:" (.getMessage t))))))
 
+;; Inline version
+(comment (defn prepare-context [context]
+           (let [base (state/get-context)
+                 svgs (core/update-all-svgs base)
+                 base-path (System/getProperty "user.dir") ; or wherever your images are relative to
+                 svg-strings (into {}
+                                   (map (fn [[k svg]]
+                                          [k (-> svg
+                                                 (svg/inline-images base-path) ; Inline the images first
+                                                 svg/hickory->svg-string)]) ; Then convert to string
+                                        svgs))]
+             (assoc base :svgs svg-strings))))
+
 (defn prepare-context [context]
   (let [base (state/get-context)
         svgs (core/update-all-svgs base)
